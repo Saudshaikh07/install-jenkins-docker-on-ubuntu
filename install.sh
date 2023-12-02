ce343227edf545558280378ac971b9d2ea5b0ffa
Saudshaikh07 <48210809+Saudshaikh07@users.noreply.github.com>
2023-12-02 15:54:22 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,26 @@
+sudo apt update
+sudo apt install openjdk-11-jdk -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins -y
 sudo apt-get update
-sudo apt-get install -y ansible
-git clone https://github.com/Saudshaikh07/setup-jenkins-ubuntu20.git
-sudo ansible-playbook setup-jenkins-ubuntu20/setup-jenkins.yaml
-sudo ansible-playbook setup-jenkins-ubuntu20/setup-docker.yaml
+sudo apt-get upgrade
+sudo apt remove docker-desktop
+rm -r $HOME/.docker/desktop
+sudo rm /usr/local/bin/com.docker.cli
+sudo apt purge docker-desktop
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-cache madison docker-ce | awk '{ print $3 }'
+VERSION_STRING=5:20.10.13~3-0~ubuntu-jammy
+sudo apt-get install docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-compose-plugin
+apt-cache madison docker-ce | awk '{ print $3 }'
